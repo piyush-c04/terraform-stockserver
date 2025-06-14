ddba4a5fc363b57a7c9d7204fa570bd3bb986599
piyush-c04 <143525614+piyush-c04@users.noreply.github.com>
2025-06-14 21:04:51 +0530

    added networking info in main.tf

--- src/main.tf
+++ src/main.tf
@@ -46,3 +46,19 @@ module "cloudwatch_monitoring" {
   dynamodb_table_name = module.dynamodb.table_name
   asg_name           = module.autoscaling.asg_name
 }
+
+
+module "networking" {
+  source = "./modules/networking"
+
+  vpc_name = "production-vpc"
+  cidr_block = "10.1.0.0/16"
+  azs       = ["us-east-1a", "us-east-1b"]
+
+  public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnet_cidrs = ["10.1.101.0/24", "10.1.102.0/24"]
+
+  tags = {
+    Environment = "production"
+  }
+}
